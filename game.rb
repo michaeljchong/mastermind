b9d1b296cf05f3075d0e2dd34888cac7e4c84e1f
Michael Chong <mikec0920@gmail.com>
2021-09-17 17:11:00 -0700

    Developed Board class

--- game.rb
+++ game.rb
@@ -1,45 +1,61 @@
-=begin
-Board
-  initialize
-    guess_history
-    feedback_history
-  display
-    show history of guesses and feedback
+class Board
+  def initialize
+    @guess_history = [['R','G','B','Y'], ['R','G','B','Y']]
+    @feedback_history = [['W','W','B','B'], ['B','W','B','B']]
+  end
 
-Player
-  initialize
-    name
-    points
+  def display
+    (0...@guess_history.length).each do |idx|
+      puts "Guess #{idx}: #{@guess_history[idx]} | Key #{idx}: #{@feedback_history[idx]}"
+    end
+  end
 
-Codebreaker < Player
-  guess
-    input guess and check for valid input
+  def add_guess(guess)
+    @guess_history.push(guess)
+  end
 
-Codemaker < Player
-  initialize
-    code
-  generate_code
-    code = random pattern of 4 code pegs
-  feedback(codebreaker_guess)
-    black keypegs for correct color and location
-    white keypegs for correct color but not location
+  def add_feedback(feedback)
+    @feedback_history.push(feedback)
+  end
+end
 
-Game
-  initialize
-    Board
-    Codemaker
-    codebreaker
-    number of rounds
-    score
-  play_round
-    codemaker.generate_code
-    loop 12 times until correct guess
-      guess = codebreaker.guess
-      codemaker.feedback(guess)
-      return if feedback is 4 black keypegs
-      increase codemaker score by 1
-    increase codemaker score by 1
-  play
-    loop number of rounds
-      play_round
-    declare winner
+Board.new.display
+
+# Player
+#   initialize
+#     name
+#     points
+
+# Codebreaker < Player
+#   guess
+#     input guess and check for valid input
+
+# Codemaker < Player
+#   initialize
+#     code
+#   generate_code
+#     code = random pattern of 4 code pegs
+#   feedback(codebreaker_guess)
+#     black keypegs for correct color and location
+#     white keypegs for correct color but not location
+
+# Game
+#   initialize
+#     Board
+#     Codemaker
+#     codebreaker
+#     number of rounds
+#     score
+#   play_round
+#     codemaker.generate_code
+#     loop 12 times until correct guess
+#       guess = codebreaker.guess
+#       codemaker.feedback(guess)
+#       return if feedback is 4 black keypegs
+#       increase codemaker score by 1
+#     increase codemaker score by 1
+#   play
+#     loop number of rounds
+#       play_round
+#       switch player roles
+#     declare winner
